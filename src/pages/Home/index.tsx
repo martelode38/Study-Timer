1f0a02b58d1d2fb7573264e6849f9eea5d03655c
Martelode38 <marteniers@gmail.com>
2024-06-24 23:02:14 -0300

    ,

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -42,13 +42,18 @@ export function Home(){
     const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId); 
 
     useEffect(() =>{
+        let interval: number;
         if (activeCycle){
-            setInterval(()=>{
+            interval = setInterval(()=>{
                 setAmountSecondsPassed(
                     differenceInSeconds(new Date(), activeCycle.startDate),
                 )
             
             },1000)
+        }
+
+        return()=>{
+            clearInterval(interval)
         }
     }, [activeCycle])
 
@@ -63,11 +68,12 @@ export function Home(){
 
         setCycles((state) => [...state, newCycle]); //clousures: sempre bom qnd vc precisar auterar um estado e ele depende da versão anterior, é melhor o estado ser citado em estado de função
         setActiveCycleId(newCycle.id);
+        setAmountSecondsPassed(0);
 
         reset();
     }
 
-    
+
 
     const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0; //variavel que vai converter o numero em minutos inserido pelo user em segundos
 
@@ -78,6 +84,12 @@ export function Home(){
 
     const minutes = String(minutesAmount).padStart(2,'0'); //essa função ela define um tamanho minimo para uma string e preenche o espaço q falta com u caractere que vc escolher no começo
     const seconds = String(secondsAmount).padStart(2,'0');
+
+    useEffect(() => {
+        if(activeCycle){
+        document.title = `${minutes}:${seconds}`
+        }
+    },[minutes, seconds, activeCycle]);
 
 
     const task = watch('task');
